f4c7da6aa3002515375fca03250a03608f9d3d34
Kilivnik <ekstasy1990@gmail.com>
2023-10-29 15:26:29 +0200

    add modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,24 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import s from './Modal.module.css';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeModal);
-  }
+export default function Modal({ closeModal, largeImageURL, imageRequest }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleCloseModal);
+    return () => {
+      window.removeEventListener('keydown', handleCloseModal);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModal);
-  }
-
-  closeModal = event => {
+  const handleCloseModal = event => {
     (event.key === 'Escape' || event.target === event.currentTarget) &&
-      this.props.closeModal();
+      closeModal();
   };
 
-  render() {
-    const { largeImageURL, imageRequest } = this.props;
-    return (
-      <div className={s.Overlay} onClick={this.closeModal}>
-        <div className={s.Modal}>
-          <img src={largeImageURL} alt={imageRequest} />
-        </div>
+  return (
+    <div className={s.Overlay} onClick={handleCloseModal}>
+      <div className={s.Modal}>
+        <img src={largeImageURL} alt={imageRequest} />
       </div>
-    );
-  }
+    </div>
+  );
 }
